ebd209947b9e38354eae978f4aa6482a7a7315da
richrobuxboy <SigmaaSigma2025@proton.me>
2025-08-24 20:40:29 +0200

    Update v2.lua

--- framework/v2.lua
+++ framework/v2.lua
@@ -1,19 +1,27 @@
 local framework = {
     functions = {},
-    connections = {}
+    connections = {},
+    initiated = tick()
 };
+
+writefile("sentinelLogs.txt", "sentinel initiated.\n");
+
 -- functions
 do
    framework.functions.getservice = function(...)
-        local services = {}
+        local services = {};
         for i, name in ipairs({...}) do
-            services[i] = cloneref(game:GetService(name))
-        end
-        return unpack(services)
-    end
+            services[i] = cloneref(game:GetService(name));
+        end;
+        return unpack(services);
+    end;
     getservice = framework.functions.getservice; 
     -- 
-    
+    framework.functions.logs = function(txt)
+        appendfile("sentinelLogs.txt", txt.." | "..string.format("%.3f s", tick() - framework.initiated));
+        appendfile("sentinelLogs.txt", "\n");
+    end;
+    logs = framework.functions.logs;
 end;
 --
 return framework;
