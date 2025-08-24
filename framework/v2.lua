576a444fcddfdc5188b51d29cb81e272fbd4acfb
richrobuxboy <SigmaaSigma2025@proton.me>
2025-08-24 04:38:01 +0200

    Update v2.lua

--- framework/v2.lua
+++ framework/v2.lua
@@ -1,1 +1,19 @@
-print"";
+local framework = {
+    functions = {},
+    connections = {}
+};
+-- functions
+do
+   framework.functions.getservice = function(...)
+        local services = {}
+        for i, name in ipairs({...}) do
+            services[i] = cloneref(game:GetService(name))
+        end
+        return unpack(services)
+    end
+    getservice = framework.functions.getservice; 
+    -- 
+    
+end;
+--
+return framework;
