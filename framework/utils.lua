171d4955447f1cc389d7d9df007451837f209e26
richrobuxboy <SigmaaSigma2025@proton.me>
2025-08-25 08:07:12 +0200

    Update utils.lua

--- framework/utils.lua
+++ framework/utils.lua
@@ -1,27 +1,50 @@
 local framework = {
-    functions = {},
+    modules = {},
+    instances = {},
     connections = {},
 };
 
 local init = tick();
 writefile("sentinelLogs.txt", "sentinel initiated.\n");
 
--- functions
+-- modules
+local modules = framework.modules;
 do
-   framework.functions.getservice = function(...)
+   modules.getservice = function(...)
         local services = {};
         for i, name in ipairs({...}) do
             services[i] = cloneref(game:GetService(name));
         end;
         return unpack(services);
     end;
-    getservice = framework.functions.getservice; 
+    getservice = modules.getservice; 
     -- 
-    framework.functions.logs = function(txt)
+    modules.logs = function(txt)
         appendfile("sentinelLogs.txt", txt.." | "..string.format("%.3f s", tick() - init));
         appendfile("sentinelLogs.txt", "\n");
     end;
-    logs = framework.functions.logs;
+    logs = modules.logs;
+    -- 
+    modules.instance_new = function(class, properties)
+        local _instance = Instance.new(class);
+        for _property, _value in next, properties do
+            _instance[_property] = _value;
+        end;
+        table.insert(framework.instances, _instance);
+        return _instance;
+    end;
+    instance_new = modules.instance_new;
+    --
+    modules.unload = function()
+        for _, instance in pairs(framework.instances) do
+            instance:Destroy();
+        end;
+    end;
+    unload = modules.unload;
+end;
+-- esp
+do
+    
 end;
 --
 return framework;
